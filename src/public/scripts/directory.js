edc80dcb2e565b4c6bd5aab6bc6d848b6ef745ef
codernineteen <wjsdPcks34@gmail.com>
2023-04-29 19:55:44 +0900

    Type: style Body: 간단한 css 스타일 작성 및 필요한 아이콘 저장

---
+++ src/public/scripts/directory.js
@@ -0,0 +1,28 @@
+const directories = document.querySelectorAll(".directory-name");
+const backButton = document.querySelector("#back");
+
+directories.forEach((dir) => {
+  dir.addEventListener("dblclick", () => {
+    const directoryName = dir.innerHTML; // 현재 디렉토리 이름
+    //서버로 request 보내는 방법 - axios 사용
+    axios
+      .post("/dirs/forward", { dirName: directoryName })
+      .then((res) => {
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        switch (err.response.data) {
+          case "ENOENT":
+            alert("No such file or directory");
+          case "EPERM":
+            alert("No permission to access this directory");
+        }
+      });
+  });
+});
+
+backButton.addEventListener("click", () => {
+  axios.get("/dirs/backward").then((res) => {
+    window.location.href = "/";
+  });
+});
