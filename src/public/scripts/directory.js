557b37c951422a97d173b110537bd15c6c208597
codernineteen <wjsdPcks34@gmail.com>
2023-05-02 20:05:07 +0900

    Type: styles/add icons
    Body: icon 추가

--- src/public/scripts/directory.js
+++ src/public/scripts/directory.js
@@ -1,9 +1,9 @@
-const directories = document.querySelectorAll(".directory-name");
+const directories = document.querySelectorAll(".directory-item");
 const backButton = document.querySelector("#back");
 
 directories.forEach((dir) => {
   dir.addEventListener("dblclick", () => {
-    const directoryName = dir.innerHTML; // 현재 디렉토리 이름
+    const directoryName = dir.childNodes[2].innerHTML; // 현재 디렉토리 이름
     //서버로 request 보내는 방법 - axios 사용
     axios
       .post("/dirs/forward", { dirName: directoryName })
