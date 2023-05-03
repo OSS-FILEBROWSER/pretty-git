d2ac30d377aa37ed5e947228ce9a5951aaac7a21
dogmania <hj296914@gmail.com>
2023-05-03 18:06:31 +0900

    Type: feat/context menu popup
    Body: 폴더에 우클릭 시 context menu 생성

--- src/public/scripts/directory.js
+++ src/public/scripts/directory.js
@@ -26,3 +26,79 @@ backButton.addEventListener("click", () => {
     window.location.href = "/";
   });
 });
+
+function handleCreateContextMenu(event){
+  // 기본 Context Menu가 나오지 않게 차단
+  event.preventDefault();
+  
+  const ctxMenu = document.createElement('div');
+  
+  ctxMenu.id = 'context-menu';
+  ctxMenu.className = 'custom-context-menu';
+  
+  //위치 설정
+  ctxMenu.style.top = event.pageY+'px';
+  ctxMenu.style.left = event.pageX+'px';
+  
+  // 메뉴 목록 생성
+  ctxMenu.appendChild(renderContextMenuList([
+    {
+     label: "git init",
+      onClick: function(event){
+        //클릭 이벤트 구현
+      }
+    },
+    {
+     label: "git commit",
+      onClick: function(event){
+        //클릭 이벤트 구현
+      }
+    },
+  ]));
+  
+  // 이전 Element 삭제
+  const prevCtxMenu = document.getElementById( 'context-menu' );
+  if( prevCtxMenu ){
+    prevCtxMenu.remove();
+  }
+  
+  // Body에 Context Menu를 추가.
+  document.body.appendChild( ctxMenu );
+}
+
+// context menu 없애기
+function handleClearContextMenu(event){
+  const ctxMenu = document.getElementById('context_menu');
+  if( ctxMenu ){
+    ctxMenu.remove();
+  }
+}
+
+function renderContextMenuList( list ){
+  // List Element 생성
+  const ctxMenuList = document.createElement('ul');
+  
+  // List Item 생성
+  list.forEach(function( item ){
+    // Item Element 생성
+    const ctxMenuItem = document.createElement('li');
+    const ctxMenuItem_a = document.createElement('a');
+    const ctxMenuItem_a_text = document.createTextNode(item.label);
+    
+    // 클릭 이벤트 설정
+    if( item.onClick ){
+      ctxMenuItem.addEventListener( 'click', item.onClick, false );
+    }
+
+    // Item 추가
+    ctxMenuItem_a.appendChild( ctxMenuItem_a_text );
+    ctxMenuItem.appendChild( ctxMenuItem_a );
+    ctxMenuList.appendChild( ctxMenuItem );
+  });
+  
+  // List Element 반환
+  return ctxMenuList;
+}
+
+document.addEventListener('contextmenu', handleCreateContextMenu, false);
+document.addEventListener('click', handleClearContextMenu, false);
