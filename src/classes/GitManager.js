c97aff604958c85d73b9566039e86123c3c27126
codernineteen <wjsdPcks34@gmail.com>
2023-05-06 21:43:45 +0900

    Type: feat/add status type
    Body: status별 상태 추가. ex) staged(deleted, new) / unstaged(modified)

--- src/classes/GitManager.js
+++ src/classes/GitManager.js
@@ -2,36 +2,42 @@ class GitManager {
   constructor(repoPath) {
     this._repoPath = repoPath;
     this._branch = null;
-    this._commits = null;
+    this._committed = false;
     this._staged = [];
     this._unstaged = []; // ex. {name: "new.txt", status: "modified"}
     this._untracked = [];
   }
 
-  updateStatus(statusLog) {
+  updateStatus(statusLog, path) {
+    this._repoPath = path;
     const lines = statusLog.toString().split("\n");
 
     for (let i = 0; i < lines.length; i++) {
       const line = lines[i].trim();
-      console.log(line);
 
       if (line.startsWith("On branch ")) {
         this._branch = line.substring("On branch ".length).trim();
+      } else if (line.startsWith("nothing to commit, working tree clean")) {
+        this._committed = true;
       } else if (line.startsWith("No commits yet")) {
-        this._commits = 0;
+        this._committed = false;
       } else if (line.startsWith("Changes to be committed:")) {
         i += 2; // Skip the next line, which is a header
         while (i < lines.length && lines[i] != "") {
-          const file = lines[i].split(":")[1].trim();
-          this._staged.push(file);
+          const info = lines[i].split(":");
+          const type = info[0].trim();
+          const name = info[1].trim();
+          this._staged.push({ type: type, name: name });
           i++;
         }
         i--; // Go back one line so we don't skip any lines
       } else if (line.startsWith("Changes not staged for commit:")) {
         i += 3; // Skip the next line, which is a header
         while (i < lines.length && lines[i] != "") {
-          const file = lines[i].split(":")[1].trim();
-          this._unstaged.push(file);
+          const info = lines[i].split(":");
+          const type = info[0].trim();
+          const name = info[1].trim();
+          this._unstaged.push({ type: type, name: name });
           i++;
         }
         i--; // Go back one line so we don't skip any lines
@@ -54,12 +60,28 @@ class GitManager {
 
     Repository path : ${this._repoPath}
     Branch : ${this._branch}
-    Commit count : ${this._commits}
+    Commit history : ${this._committed}
     Staged items : ${this._staged}
     Unstaged items(being tracked) : ${this._unstaged}
     Untracked items : ${this._untracked}
     `
     );
+  }
+
+  get branch() {
+    return this._branch;
+  }
+
+  get staged() {
+    return this._staged;
+  }
+
+  get unstaged() {
+    return this._unstaged;
+  }
+
+  get untracked() {
+    return this._untracked;
   }
 }
 
